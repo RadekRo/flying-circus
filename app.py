0917144f6572a6fec4f70ce43a757870f5f95f17
RadekRo <r.roclawski@ankara.pl>
2023-05-06 14:26:20 +0200

    Created basic index.html page with basic txt

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask
+from flask import render_template, request, redirect
 import data
+import bcrypt
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
 
 # put your code here
