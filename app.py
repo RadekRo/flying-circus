da4636ef2b9b9655924a61f76fe26605130dd6e2
RadekRo <r.roclawski@ankara.pl>
2023-05-06 18:12:48 +0200

    Proper password repeat check inplemented

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask import render_template, request, redirect
-import data
-import bcrypt
 
 app = Flask(__name__)
 
@@ -11,7 +9,20 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        pass
+
+@app.route('/sign-in', methods=['GET', 'POST'])
+def sign_in():
+    if request.method == "GET":
+        return render_template("sign-in.html")
+    else:
+        login = request.form.get('login')
+        login_repeat = request.form.get('login-repeat')
+        if login != login_repeat:
+            return render_template("sign-in.html", error = "wrong-login-repeat")
 
 if __name__ == '__main__':
     app.run()
